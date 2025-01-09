f3fa8695fc42bc4a12e5bb18f7cc17ad25c016c3
cispl-suvendu <suvendu.chatterjee@codeclouds.in>
2025-01-09 16:06:36 +0530

    added tab

--- src/components/assignment/index.tsx
+++ src/components/assignment/index.tsx
@@ -1,15 +1,30 @@
 'use client'
-import React, { useState } from 'react'
+import React, { Suspense, useState } from 'react'
 const AssignExamCard = React.lazy(() => import('../card/assignExamCard'));
 import Pagination from '../pagination';
+import Skeleton from '../skeleton';
+import { useRouter, useSearchParams } from 'next/navigation';
 
 export default function AllAssignment({ allAssignedExam }: any) {
+    const router = useRouter()
     const [page, setPage] = useState<number>(1)
     const [limit, setLimit] = useState<number>(5)
+     const searchParams = useSearchParams();
+    const activeStatus = searchParams.get("query")
     return (
         <div>
-            <AssignExamCard assignedExam={allAssignedExam} setPage={setPage} setLimit={setLimit} pageLimit={limit} showPagination={false} />
-            <Pagination pagination={allAssignedExam.pagination} />
+            <Suspense fallback={<Skeleton />}>
+                <div>
+                    <div className='flex gap-2'>
+                        <div className={`${activeStatus === "completed" ? 'bg-supportingMegenda text-white' : 'bg-white'} px-4 py-2 cursor-pointer capitalize rounded`} onClick={() => router.replace('/assignment?query=completed')}>Completed</div>
+                        <div className={`${activeStatus === "assigned" ? 'bg-supportingMegenda text-white' : 'bg-white'} px-4 py-2 cursor-pointer capitalize rounded`} onClick={() => router.replace('/assignment?query=assigned')}>assigned</div>
+                    </div>
+                    <AssignExamCard assignedExam={allAssignedExam} setPage={setPage} setLimit={setLimit} pageLimit={limit} showPagination={false} />
+                </div>
+            </Suspense>
+            <Suspense fallback={<Skeleton />}>
+                <Pagination pagination={allAssignedExam.pagination} />
+            </Suspense>
         </div>
     )
 }
